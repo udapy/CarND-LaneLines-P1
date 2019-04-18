311780e7821f5d59831381419b03df87c7085d31
udapy <udayphlk@gmail.com>
2019-04-18 00:09:46 +0000

    Tuned video challenge

--- set_git.sh
+++ set_git.sh
@@ -6,20 +6,20 @@ git pull
 echo
 
 # Ask the user for login details
-read -p 'Git repository url: ' upstreamVar
-read -p 'Git Username: ' userVar
-read -p 'Git email: ' emailVar
+#read -p 'Git repository url: ' upstreamVar
+#read -p 'Git Username: ' userVar
+#read -p 'Git email: ' emailVar
 
 echo
-echo Thank you $userVar!, we now have your credentials
-echo for upstream $upstreamVar. You must supply your password for each push.
+echo Thank you userVar!, we now have your credentials
+echo for upstream upstreamVar. You must supply your password for each push.
 echo
 
 echo setting up git
 
-git config --global user.name $userVar
-git config --global user.email $emailVar
-git remote set-url origin $upstreamVar
+git config --global user.name udapy
+git config --global user.email udayphlk@gmail.com
+git remote set-url origin https://github.com/udapy/CarND-LaneLines-P1
 echo
 
 echo Please verify remote:
